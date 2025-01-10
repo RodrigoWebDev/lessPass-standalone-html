521335bff1eb9a6110a24ae2fd73dfddf7a064e0
RodrigoWebDev <rodrigo11397@protonmail.com>
2025-01-10 12:36:17 -0300

    feat: download link

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import lesspass from "lesspass";
 const getById = (el) => document.getElementById(el);
 
 const myForm = getById("form");
+const downloadLink = getById("download");
 
 const copyPass = (pass) => {
   const copyText = getById("hiddenPass");
@@ -10,16 +11,16 @@ const copyPass = (pass) => {
   copyText.select();
   copyText.setSelectionRange(0, 99999);
   navigator.clipboard.writeText(copyText.value);
-  copyText.value = ""
+  copyText.value = "";
 };
 
 const showSnackBar = () => {
   var snackbar = document.getElementById("snackbar");
   snackbar.className = "show";
-  setTimeout(function(){ 
+  setTimeout(function () {
     snackbar.className = "";
   }, 3000);
-}
+};
 
 myForm.addEventListener("submit", async (e) => {
   e.preventDefault();
@@ -42,8 +43,21 @@ myForm.addEventListener("submit", async (e) => {
     );
     copyPass(pass);
 
-    showSnackBar()
+    showSnackBar();
   } else {
     alert("Select at least one checkbox");
   }
 });
+
+downloadLink.addEventListener("click", () => {
+  save()
+});
+
+function save() {
+  const data = "<!DOCTYPE html>\n"+document.documentElement.outerHTML; // See sources below for why this gets the entire page content.
+  const link = document.createElement('a');
+  link.setAttribute('download', 'default_download_name.html');
+  link.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(data));
+  link.click(); // In my tests, there was no need to add the element to the document for this to work.
+}
+
