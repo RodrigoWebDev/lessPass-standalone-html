9cda739ebfe19838aff6df8431eac632f58c59b0
RodrigoWebDev <rodrigo11397@protonmail.com>
2025-01-10 11:44:23 -0300

    feat: snackbar

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import lesspass from "lesspass";
+
+const getById = (el) => document.getElementById(el);
+
+const myForm = getById("form");
+
+const copyPass = (pass) => {
+  const copyText = getById("hiddenPass");
+  copyText.value = pass;
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+  copyText.value = ""
+};
+
+const showSnackBar = () => {
+  var snackbar = document.getElementById("snackbar");
+  snackbar.className = "show";
+  setTimeout(function(){ 
+    snackbar.className = "";
+  }, 3000);
+}
+
+myForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const formValues = Object.fromEntries(formData);
+
+  if (
+    formValues.lowercase ||
+    formValues.uppercase ||
+    formValues.digits ||
+    formValues.symbols
+  ) {
+    const passwordProfile = {
+      ...formValues,
+    };
+
+    const pass = await lesspass.generatePassword(
+      passwordProfile,
+      formValues["password"]
+    );
+    copyPass(pass);
+
+    showSnackBar()
+  } else {
+    alert("Select at least one checkbox");
+  }
+});
